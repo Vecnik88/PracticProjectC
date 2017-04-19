590eb195da62bf25fe11184857de7bd7a6922842
Vecnik88 <Ve_Cni_K@inbox.ru>
2017-04-19 12:22:12 +0300

    Update read.c

--- raw_socket/read.c
+++ raw_socket/read.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define BUFFER_SIZE 1024
 
@@ -39,7 +40,7 @@ unsigned short csum(unsigned short* buf, int nwords)
 	for(sum = 0; nwords > 0; --nwords)
 		sum += *buf++;
 
-	sum = (sum >> 16) + (sum &0xffff);
+	sum = (sum >> 16) + (sum & 0xffff);
 	sum += (sum >> 16);
 
 	return (unsigned short)(~sum);
@@ -52,12 +53,13 @@ int main(void)
 	int i;
 	
 	char buffer[BUFFER_SIZE];
-	char packet[50];
+
+	srand(time(NULL));
 
 	ip_header* ip = (ip_header*) buffer;
 	udp_header* udp = (udp_header*)(buffer + sizeof(ip_header));
 
-	struct sockaddr_in message_to, message_from;
+	struct sockaddr_in message_to;
 	int one = 1;
 	const int *val = &one;
 
@@ -67,60 +69,41 @@ int main(void)
 		errorMessage("socket");
 
 	message_to.sin_family = AF_INET;
-	message_from.sin_family = AF_INET;
-
+	
 	message_to.sin_port = htons(12345);
-	message_from.sin_port = htons(1234);
 
-	inet_pton(AF_INET, "127.0.0.1", &(message_to.sin_addr));
-	inet_pton(AF_INET, "127.0.0.1", &(message_from.sin_addr));
+	inet_pton(AF_INET, "127.0.0.1",(struct in_addr *) &(message_to.sin_addr.s_addr));
 
 	ip->iph_ihl = 5;
 	ip->iph_ver = 4;
 	ip->iph_tos = 16;
 	ip->iph_len = sizeof(ip_header) + sizeof(udp_header);
-	ip->iph_ident = htons(54321);
+	ip->iph_ident = htons(rand()%65000);
 	ip->iph_ttl = 64;
-	ip->iph_protocol = 17;
-	ip->iph_sourceip = message_to.sin_addr.s_addr;
-	ip->iph_destip = message_from.sin_addr.s_addr;
+	ip->iph_protocol = IPPROTO_UDP;
+	ip->iph_sourceip = htonl(message_to.sin_addr.s_addr);
+	ip->iph_destip = htonl(message_to.sin_addr.s_addr);
+	ip->iph_chksum = 0;
 
 	udp->udph_srcport = htons(1234);
 	udp->udph_destport = htons(12345);
 
-	udp->udph_len = htons(sizeof(struct udpheader));
-	udp->udph_chksum = csum((unsigned short*)buffer, sizeof(ip_header) + sizeof(udp_header));
-	setsockopt(raw_socket_udp, IPPROTO_IP, IP_HDRINCL, val, sizeof(one));
+	udp->udph_len = htons(sizeof(udp_header));
+	udp->udph_chksum = 0;//csum((unsigned short*)buffer, sizeof(ip_header) + sizeof(udp_header));
+	//setsockopt(raw_socket_udp, IPPROTO_IP, IP_HDRINCL, val, sizeof(one));
 
-
-	memset(packet, 0, sizeof(packet));
 	memset(buffer, 0, sizeof(buffer));
 
-	fromlen = sizeof(message_from);
+	strcpy(buffer, "Hello world");
 
 	while(1) 
 	{
-		printf("1\n");
-
 		if(sendto(raw_socket_udp, buffer, sizeof(buffer), MSG_NOSIGNAL,
 			(struct sockaddr*)&message_to, sizeof(message_to)) < 0)
 			errorMessage("sendto");
 
-		printf("2\n");
-
-		if (recvfrom(raw_socket_udp, (char *)&packet, sizeof(packet), 0,
-			(struct sockaddr *)&message_from, &fromlen) < 0)
-			perror("packet receive error:");
-
-		i = sizeof(struct iphdr);
-		printf("3\n");
-
-		while (i < sizeof(packet)) 
-		{
-			fprintf(stdout, "%c", packet[i]);
-			i++;
-		}
-		printf("\n");
+		printf("Send %s\n", buffer);
+		sleep(1);
 	}
 	exit(EXIT_SUCCESS);
 }
